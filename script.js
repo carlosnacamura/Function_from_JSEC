d20befc558a3d7d17b447d73654377b7b15779fa
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-05-04 23:14:32 -0300

    Tarefa

---
+++ script.js
@@ -0,0 +1,12 @@
+const box = document.querySelector(".box_images")
+const imagens = document.querySelectorAll(".box_images img")
+let contador = 0
+function slider(){
+    contador++
+    if (contador > imagens.length - 1){
+        contador = 0
+    }
+    box.style.transform = `translateX(${-contador * 100}vw)`
+}
+setInterval(slider, 4000)
+
